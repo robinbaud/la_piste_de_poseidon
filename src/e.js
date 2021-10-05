2881b7589616caf05045f9858d5703d1d8efdfc2
robinbaud <72597879+robinbaud@users.noreply.github.com>
2021-10-05 10:38:42 +0200

    Update e.js

--- src/e.js
+++ src/e.js
@@ -4,8 +4,16 @@ const E = () =>{
     return(
         <div className="page">
             <h2 className="message">
-                Faites demi-tour pour trouver le dernier indice.
+                Mais ceci n'est qu'une histoire
             </h2>
+            <p>une telle legende a été inventé pour trompé les voyageur vereux ou avide
+                mais vous, vous m'avez amplement montrer votre confiance et votre courage.
+                je vous attends a l'oasis,
+                je me nomme Poseidon 
+                retrouver moi dans le dernier lieu et commandez-moi de l'eau
+                vous devez en avoir besoin apres ce long voyage.
+            </p>
+            <p>faite demi tour pour trouver le lieu secret</p>
         </div> 
     );
 }
